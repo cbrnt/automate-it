094ff797a522f3df48eee32c22f695e77ed728a0
Вигерин Эдуард <vigerin@tochkak.ru>
2021-11-29 19:15:27 +0300

    добавил запрос на создание в jira, создал классы, вынес списки в отдельный файл

---
+++ lists.py
@@ -0,0 +1,26 @@
+
+position_dict = {
+    'developer': 'Разработчик',
+    'vis': 'Оператор ВИС',
+    'trader': 'Менеджер по продажам',
+    'qa': 'Тестировщик',
+    'scrum': 'SCRUM-мастер',
+    'bill': 'Бухгалтер',
+    'backend': 'Backend разработчик',
+    'frontend': 'Frontend разработчик',
+    'writer': 'Технический писатель',
+    'manager': 'Менеджер продуктов',
+    'sysadmin': 'Системный администратор'
+}
+
+jira_groups_dict = {
+    'developer': [
+        'confluence-users', 'kinoplan_onboarding', 'stash-users'
+    ],
+    'backend': [
+        'confluence-users', 'kinoplan_onboarding', 'stash-users'
+    ],
+    'frontend': [
+        'confluence-users', 'kinoplan_onboarding', 'stash-users'
+    ],
+}
--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 from unidecode import unidecode
 import requests
+import base64
+
+import lists
+
 
 
 global JIRA_HOST
 
+DEBUG = True
+
 JIRA_HOST = 'http://10.0.0.7:8080/'
-JIRA_CREDENTIALS = 'vigerin:wantt0Know'
+jira_credentials = 'vigerin:wantt0Know'
+jira_credentials_bytes = jira_credentials.encode('ascii')
+JIRA_CREDENTIALS_BASE64 = base64.b64encode(jira_credentials_bytes).decode('ascii')
 
 
-position_dict = {
-    'developer': 'Разработчик',
-    'vis': 'Оператор ВИС',
-    'trader': 'Менеджер по продажам',
-    'qa': 'Тестировщик',
-    'scrum': 'SCRUM-мастер',
-    'bill': 'Бухгалтер',
-    'backend': 'Backend разработчик',
-    'frontend': 'Frontend разработчик',
-    'writer': 'Технический писатель',
-    'manager': 'Менеджер продуктов',
-    'sysadmin': 'Системный администратор'
-}
-
-jira_groups_dict = {
-    'developer': [
-        'confluence-users', 'kinoplan_onboarding', 'stash-users'
-    ],
-    'backend': [
-        'confluence-users', 'kinoplan_onboarding', 'stash-users'
-    ],
-    'frontend': [
-        'confluence-users', 'kinoplan_onboarding', 'stash-users'
-    ],
-}
+employee_name = 'Эдуард'
+employee_surname = '               Вигерин'
+employee_domain = 'tochkak.ru'
+employee_position = 'sysadmin'
 
 mail_groups  = {
     'vis': [
@@ -40,35 +27,75 @@ mail_groups  = {
     ]
 }
 
-class Employee:
-    def __init__(self, name, surname, mail, position):
-        self.name = name
-        self.latin_name = unidecode(name)
-        self.surname = surname
-        self.latin_surname = unidecode(surname)
-        self.mail = mail
-        self.position = position
+
+class Google:
+    def create(self):
+        token = ''
+
 
 class Jira:
-    def check_user_exist(self, mail):
-        api_call = 'rest/api/2/user/search'
-        headers = {'Content-Type': 'application/json'}
-        params = {
-            'username': '%s' % mail,
-            'maxResults': 1
-        }
-        get_user_list = requests.get(url=JIRA_HOST + api_call, headers=headers, params=params)
-        print(get_user_list.headers)
-        print(get_user_list.text)
 
+    def create(self):
+        api_call = 'rest/api/2/user'
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Basic %s' % JIRA_CREDENTIALS_BASE64}
+        if DEBUG:
+            print('headers = ', headers)
+        data = {
+            "name": self.latin_surname,
+            "password": '',
+            "emailAddress": self.mail,
+            "displayName": self.surname + ' ' + self.name,
+            "applicationKeys": [
+            "jira-core"
+            ]
+        }
+        if DEBUG:
+            print('Params: ', self.latin_name, self.mail, self.latin_surname,self.surname + ' ' + self.name)
+        create_user_request = requests.post(url=JIRA_HOST + api_call, headers=headers, json=data)
+        if DEBUG:
+            print(create_user_request.status_code)
+            print(create_user_request.headers)
+            print(create_user_request.text)
+        if create_user_request.status_code == 201:
+            if DEBUG:
+                print("Пользователь создан в Jira")
+            else:
+                print("Пользователь не создан в Jira")
+            return True
+
+class Slack:
+    def create(self, mail):
+        return True
+
+# Physical Access Control System, СКУД
+class PACS:
+    def create(self, name, surname, position, card_it):
+        return True
+
+class login:
+    def create(self, name, surname, mail, position):
+        return True
+
+
+class Employee:
+    def __init__(self, name, surname, domain, position):
+        self.name = name.replace(' ', '')
+        self.latin_name = unidecode(self.name).lower()
+        self.surname = surname.replace(' ', '')
+        self.latin_surname = unidecode(self.surname).lower()
+        self.mail = self.latin_surname + '@' + domain
+        if DEBUG:
+            print('Будущая почта сотрудника: ', self.mail)
+        self.position = position
 
 
 
 def main():
-    new_employee = Employee('Эдуард', 'Вигерин', 'tochkak.ru', 'sysadmin')
-    print(new_employee)
-    jira_check = Jira()
-    jira_check.check_user_exist('vigeri@tochkak.ru')
+
+    new_employee = Employee(employee_name, employee_surname, employee_domain, employee_position)
+    new_jira_user = Jira.create(new_employee)
+
+
 
 
 
